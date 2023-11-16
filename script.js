a48a122baaee2edb74e781da0e5ec1a81de909e9
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-15 23:37:05 -0800

    added responsive comment

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const array = [3, 5, 17, -4, 6]
 console.log(array[0])
 console.log(array[4])
+// I passed from no indexes for the first statement to passing through 4 indexes for the last, the reason being that for computations, the numbering starts at 0.
