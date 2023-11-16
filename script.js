ff40d3bef9210fc7b8ba2f317340ede69c4fa16c
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-15 23:47:30 -0800

    second for loop attempt

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log(array[0])
 console.log(array[4])
 // I passed from no indexes for the first statement to passing through 4 indexes for the last, the reason being that for computations, the numbering starts at 0.
 for (let numbers of array){
-    if (numbers = [0]) numbers + [1]
-    console.log(numbers) 
+    if (numbers = [0]) {
+        console.log([0+1])
+    }
 }
