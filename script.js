9704d179c15bad47468f44dfe0754ac5e651063e
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-16 00:24:59 -0800

    3rd question for loop response attempt

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-const array = [3, 5, 17, -4, 6]
+const array = [3, 5, 17, -4, 6];
 console.log(array[0])
 console.log(array[4])
 // I passed from no indexes for the first statement to passing through 4 indexes for the last, the reason being that for computations, the numbering starts at 0.
-for (let numbers of array){
-    if (numbers = [0]) {
-        console.log([0+1])
-    }
+for (let i = 0; i < array.length; i++){
+    console.log(array[i]);
 }
